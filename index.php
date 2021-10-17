32dc42ffef1d213bc7dcb4ce127848b7a3456927
DESKTOP-HIGNV92\Bijay <bijaythakur330@gmail.com>
2021-10-17 08:23:48 +0545

    The basic blocks of a wordpress theme part 1

--- index.php
+++ index.php
@@ -6,7 +6,34 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Fancy LaB</title>
 </head>
-<body>
-  
+<body <?php body_class(); ?> >
+  <div id="page" class="site">
+    <header>
+      <section class="search">Search</section>
+      <section class="top-bar">
+        <div class="brand">Logo</div>
+        <div class="second-column">
+          <div class="account">Account</div>
+          <nav class="main-menu">Menu</nav>
+        </div>
+      </section>
+    </header>
+
+    <div class="content-area">
+      <main>
+        <section class="slider">Slider</section>
+        <section class="popular-products">Popular Products</section>
+        <section class="new-arrivals">New Arrivals</section>
+        <section class="deal-of-the-week">Deal of the week</section>
+        <section class="lab-blog">News</section>
+      </main>
+    </div>
+
+    <footer>
+      <section class="footer-widgets">Footer Widgets</section>
+      <section class="copyright">Copyright</section>
+    </footer>
+
+  </div>
 </body>
 </html>
