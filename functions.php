931e2b868c4f66895596cab97bbf7ece7aeefb61
DESKTOP-HIGNV92\Bijay <bijaythakur330@gmail.com>
2021-10-22 17:52:10 +0545

    creating_menus_part2

--- functions.php
+++ functions.php
@@ -8,6 +8,7 @@
  * @package Fancy Lab
  */
 
+//  libraries adding start
  function fancy_lab_scripts() {
   //  wp_enqueue_style( 'fancy-lab-style', get_stylesheet_uri(), array(), '1.0', 'all' ); // for deployment
 
@@ -16,6 +17,19 @@
    wp_enqueue_style( 'bootstrap-css', get_template_directory_uri() . '/inc/bootstrap.min.css', array(), '4.3.1', 'all' );
    wp_enqueue_script( 'bootstrap-js', get_template_directory_uri() . '/inc/bootstrap.min.js', array( 'jquery' ), '4.3.1', true );
 
-
  }
  add_action( 'wp_enqueue_scripts', 'fancy_lab_scripts' );
+
+//  libraries adding end
+
+// menu start
+ function fancy_lab_config() {
+    register_nav_menus(
+      array(
+        'fancy_lab_main_menu' => 'Fancy Lab Main Menu'
+      )
+    );
+ }
+ add_action( 'after_setup_theme', 'fancy_lab_config', 0 );
+
+//  menu end
