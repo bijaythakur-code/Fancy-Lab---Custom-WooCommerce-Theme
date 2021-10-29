3e8040e338e483d108da71e47af978eabbd64170
DESKTOP-HIGNV92\Bijay <bijaythakur330@gmail.com>
2021-10-29 10:27:15 +0545

    adding a logo to our theme

--- functions.php
+++ functions.php
@@ -8,89 +8,98 @@
  * @package Fancy Lab
  */
 
- /**
+/**
  * Register Custom Navigation Walker
  */
-function register_navwalker(){
-	require_once get_template_directory() . '/inc/class-wp-bootstrap-navwalker.php';
+function register_navwalker()
+{
+  require_once get_template_directory() . '/inc/class-wp-bootstrap-navwalker.php';
 }
-add_action( 'after_setup_theme', 'register_navwalker' );
+add_action('after_setup_theme', 'register_navwalker');
 
 //  libraries adding start
- function fancy_lab_scripts() {
+function fancy_lab_scripts()
+{
 
   // Bootstrap javascript and CSS files
-  wp_enqueue_style( 'bootstrap-css', get_template_directory_uri() . '/inc/bootstrap.min.css', array(), '4.3.1', 'all' );
-  wp_enqueue_script( 'bootstrap-js', get_template_directory_uri() . '/inc/bootstrap.min.js', array( 'jquery' ), '4.3.1', true );
+  wp_enqueue_style('bootstrap-css', get_template_directory_uri() . '/inc/bootstrap.min.css', array(), '4.3.1', 'all');
+  wp_enqueue_script('bootstrap-js', get_template_directory_uri() . '/inc/bootstrap.min.js', array('jquery'), '4.3.1', true);
 
   // Theme's main stylesheet
   //  wp_enqueue_style( 'fancy-lab-style', get_stylesheet_uri(), array(), '1.0', 'all' ); // for deployment
-   wp_enqueue_style( 'fancy-lab-style', get_stylesheet_uri(), array(), filemtime( get_template_directory() . '/style.css' ), 'all' ); // for development environment
+  wp_enqueue_style('fancy-lab-style', get_stylesheet_uri(), array(), filemtime(get_template_directory() . '/style.css'), 'all'); // for development environment
 
   //  Google Fonts
-  wp_enqueue_style( 'google-fonts', 'https://fonts.googleapis.com/css2?family=Rajdhani:wght@400;500;600;700&family=Seaweed+Script&display=swap' );
-  
-
- }
- add_action( 'wp_enqueue_scripts', 'fancy_lab_scripts' );
+  wp_enqueue_style('google-fonts', 'https://fonts.googleapis.com/css2?family=Rajdhani:wght@400;500;600;700&family=Seaweed+Script&display=swap');
+}
+add_action('wp_enqueue_scripts', 'fancy_lab_scripts');
 
 //  libraries adding end
 
 // menu start
- function fancy_lab_config() {
-    register_nav_menus(
-      array(
-        'fancy_lab_main_menu' => 'Fancy Lab Main Menu',
-        'fancy_lab_footer_menu' => 'Fancy Lab Footer Menu',
-      )
-    );
+function fancy_lab_config()
+{
+  register_nav_menus(
+    array(
+      'fancy_lab_main_menu' => 'Fancy Lab Main Menu',
+      'fancy_lab_footer_menu' => 'Fancy Lab Footer Menu',
+    )
+  );
 
-    add_theme_support( 'woocommerce', array(
-      'thumbnail_image_width' => 255,
-      'single_image_width'    => 255,
-      'product_grid'          => array(
-        'default_rows'      => 10,
-        'min_rows'          => 5,
-        'max_rows'          => 10,
-        'default_columns'   => 1,
-        'min_columns'       => 1,
-        'max_columns'       => 1,
+  add_theme_support('woocommerce', array(
+    'thumbnail_image_width' => 255,
+    'single_image_width'    => 255,
+    'product_grid'          => array(
+      'default_rows'      => 10,
+      'min_rows'          => 5,
+      'max_rows'          => 10,
+      'default_columns'   => 1,
+      'min_columns'       => 1,
+      'max_columns'       => 1,
 
-      )
-    ) );
-    add_theme_support( 'wc-product-gallery-zoom' );
-    add_theme_support( 'wc-product-gallery-lightbox' );
-    add_theme_support( 'wc-product-gallery-slider' );
+    )
+  ));
+  add_theme_support('wc-product-gallery-zoom');
+  add_theme_support('wc-product-gallery-lightbox');
+  add_theme_support('wc-product-gallery-slider');
 
-    if ( ! isset( $content_width ) ) {
-      $content_width = 600;
-    }
- }
- add_action( 'after_setup_theme', 'fancy_lab_config', 0 );
+  add_theme_support('custom-logo', array(
+    'height'    => 85,
+    'width'     => 160,
+    'flex_height'   => true,
+    'flex_width'    => true,
+  ));
+
+  if (!isset($content_width)) {
+    $content_width = 600;
+  }
+}
+add_action('after_setup_theme', 'fancy_lab_config', 0);
 //  menu end
 
 /**
  * If WooCommerce is active, we want to enqueue a file
  * with a couple of template overrides
  */
-if( class_exists( 'WooCommerce' ) ){
-  require get_template_directory(). '/inc/wc-modifications.php';
+if (class_exists('WooCommerce')) {
+  require get_template_directory() . '/inc/wc-modifications.php';
 }
 
 
 /**
  * Show cart contents / total Ajax
  */
-add_filter( 'woocommerce_add_to_cart_fragments', 'fancy_lab_woocommerce_header_add_to_cart_fragment' );
+add_filter('woocommerce_add_to_cart_fragments', 'fancy_lab_woocommerce_header_add_to_cart_fragment');
 
-function fancy_lab_woocommerce_header_add_to_cart_fragment( $fragments ) {
-	global $woocommerce;
+function fancy_lab_woocommerce_header_add_to_cart_fragment($fragments)
+{
+  global $woocommerce;
 
-	ob_start();
+  ob_start();
 
-	?>
-	<span class="items"><?php echo WC()->cart->get_cart_contents_count(); ?></span>
-	<?php
-	$fragments['span.items'] = ob_get_clean();
-	return $fragments;
+?>
+  <span class="items"><?php echo WC()->cart->get_cart_contents_count(); ?></span>
+<?php
+  $fragments['span.items'] = ob_get_clean();
+  return $fragments;
 }
--- header.php
+++ header.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * The header for our theme
  *
@@ -11,6 +12,7 @@
 ?>
 <!DOCTYPE html>
 <html <?php language_attributes(); ?>>
+
 <head>
   <meta charset="<?php bloginfo('charset'); ?>">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -19,7 +21,8 @@
   <?php wp_head(); ?>
 
 </head>
-<body <?php body_class(); ?> >
+
+<body <?php body_class(); ?>>
   <div id="page" class="site">
     <header>
       <section class="search">
@@ -32,23 +35,50 @@
       <section class="top-bar">
         <div class="container">
           <div class="row">
-            <div class="brand col-md-3 col-12 col-lg-2 text-center text-md-left">Logo</div>
+            <div class="brand col-md-3 col-12 col-lg-2 text-center text-md-left">
+              <a href="<?php echo home_url('/') ?>">
+                <?php if (has_custom_logo()) : ?>
+                  <?php the_custom_logo(); ?>
+                <?php else : ?>
+                  <p class="site-title"><?php bloginfo('title'); ?></p>
+                  <span><?php bloginfo('description'); ?></span>
+                <?php endif; ?>
+              </a>
+            </div>
             <div class="second-column col-md-9 col-12 col-lg-10">
               <div class="row">
-              <div class="account col-12">
-                <div class="cart text-right">
-                  <a href="<?php echo wc_get_cart_url(); ?>"><span class="cart-icon"></span></a>
-                  <span class="items"><?php echo WC()->cart->get_cart_contents_count(); ?></span>
-                </div>
-              </div>
-              <div class="col-12">
-              <nav class="main-menu navbar navbar-expand-md navbar-light" role="navigation">
-              <!-- Brand and toggle get grouped for better mobile display -->
-              <button class="navbar-toggler ml-auto" type="button" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-controls="bs-example-navbar-collapse-1" aria-expanded="false" aria-label="<?php esc_attr_e( 'Toggle navigation', 'your-theme-slug' ); ?>">
-                  <span class="navbar-toggler-icon"></span>
-              </button>
-                  <?php
-                  wp_nav_menu( array(
+                <?php if (class_exists('WooCommerce')) : ?>
+                  <div class="account col-12">
+                    <div class="navbar-expand">
+                      <ul class="navbar-nav float-left">
+                        <?php if (is_user_logged_in()) : ?>
+                          <li>
+                            <a href="<?php echo esc_url(get_permalink(get_option('woocommerce_myaccount_page_id'))) ?>" class="nav-link">My Account</a>
+                          </li>
+                          <li>
+                            <a href="<?php echo esc_url(wp_logout_url(get_permalink(get_option('woocommerce_myaccount_page_id')))) ?>" class="nav-link">Logout</a>
+                          </li>
+                        <?php else : ?>
+                          <li>
+                            <a href="<?php echo esc_url(get_permalink(get_option('woocommerce_myaccount_page_id'))) ?>" class="nav-link">Login / Register</a>
+                          </li>
+                        <?php endif; ?>
+                      </ul>
+                    </div>
+                    <div class="cart text-right">
+                      <a href="<?php echo wc_get_cart_url(); ?>"><span class="cart-icon"></span></a>
+                      <span class="items"><?php echo WC()->cart->get_cart_contents_count(); ?></span>
+                    </div>
+                  </div>
+                <?php endif; ?>
+                <div class="col-12">
+                  <nav class="main-menu navbar navbar-expand-md navbar-light" role="navigation">
+                    <!-- Brand and toggle get grouped for better mobile display -->
+                    <button class="navbar-toggler ml-auto" type="button" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-controls="bs-example-navbar-collapse-1" aria-expanded="false" aria-label="<?php esc_attr_e('Toggle navigation', 'your-theme-slug'); ?>">
+                      <span class="navbar-toggler-icon"></span>
+                    </button>
+                    <?php
+                    wp_nav_menu(array(
                       'theme_location'    => 'fancy_lab_main_menu',
                       'depth'             => 3,
                       'container'         => 'div',
@@ -57,15 +87,14 @@
                       'menu_class'        => 'nav navbar-nav',
                       'fallback_cb'       => 'WP_Bootstrap_Navwalker::fallback',
                       'walker'            => new WP_Bootstrap_Navwalker(),
-                  ) );
-                  ?>
-              </nav>
-              </div>
-            
+                    ));
+                    ?>
+                  </nav>
+                </div>
+
               </div>
             </div>
           </div>
         </div>
       </section>
     </header>
-
