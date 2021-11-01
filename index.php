47ab7168e14b2b88b612a4d21dac41378cb63a12
DESKTOP-HIGNV92\Bijay <bijaythakur330@gmail.com>
2021-11-01 17:58:44 +0545

    modifying the blog posts index template file

--- index.php
+++ index.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * The main template file
  *
@@ -14,29 +15,51 @@
 
 get_header();
 ?>
-		<div class="content-area">
-			<main>
-				<div class="container">
-					<div class="row">
-						<?php 
-							// If there are any posts
-							if( have_posts() ):
+<div class="content-area">
+	<main>
+		<div class="container">
+			<div class="row">
+				<?php
+				// If there are any posts
+				if (have_posts()) :
 
-								// Load posts loop
-								while( have_posts() ): the_post();
-									?>
-										<article>
-											<h2><?php the_title(); ?></h2>
-											<div><?php the_content(); ?></div>
-										</article>
-									<?php
-								endwhile;
-							else:
-						?>
-							<p>Nothing to display.</p>
-						<?php endif; ?>
-					</div>
-				</div>
-			</main>
+					// Load posts loop
+					while (have_posts()) : the_post();
+				?>
+						<article <?php post_class(); ?>>
+							<h2>
+								<a href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
+							</h2>
+							<div class="post-thumbnail">
+								<?php
+								if (has_post_thumbnail()) :
+									the_post_thumbnail('fancy-lab-blog', array('class' => 'img-fluid'));
+
+								endif;
+								?>
+							</div>
+							<div class="meta">
+								<p>Published by <?php the_author_posts_link(); ?> on <?php echo get_the_date(); ?>
+									<br />
+									<?php if (has_category()) : ?>
+										Categories: <span><?php the_category(' '); ?></span>
+									<?php endif; ?>
+									<br />
+									<?php if (has_tag()) : ?>
+										Tags: <span><?php the_tags('', ', ') ?></span>
+									<?php endif; ?>
+								</p>
+							</div>
+							<div><?php the_excerpt(); ?></div>
+						</article>
+					<?php
+					endwhile;
+				else :
+					?>
+					<p>Nothing to display.</p>
+				<?php endif; ?>
+			</div>
 		</div>
+	</main>
+</div>
 <?php get_footer(); ?>
