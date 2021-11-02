b87a5609123aec0c5b2d48826c042d660cdf2b79
DESKTOP-HIGNV92\Bijay <bijaythakur330@gmail.com>
2021-11-02 15:26:39 +0545

    creating inner page templates part 1 - single.php

--- page.php
+++ page.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * The template for displaying all pages
  *
@@ -14,29 +15,23 @@
 
 get_header();
 ?>
-		<div class="content-area">
-			<main>
-				<div class="container">
-					<div class="row">
-						<?php 
-							// If there are any posts
-							if( have_posts() ):
-
-								// Load posts loop
-								while( have_posts() ): the_post();
-									?>
-										<article class="col">
-											<h1><?php the_title(); ?></h1>
-											<div><?php the_content(); ?></div>
-										</article>
-									<?php
-								endwhile;
-							else:
-						?>
-							<p>Nothing to display.</p>
-						<?php endif; ?>
-					</div>
-				</div>
-			</main>
+<div class="content-area">
+	<main>
+		<div class="container">
+			<div class="row">
+				<?php
+				// Load posts loop
+				while (have_posts()) : the_post();
+				?>
+					<article class="col">
+						<h1><?php the_title(); ?></h1>
+						<div><?php the_content(); ?></div>
+					</article>
+				<?php
+				endwhile;
+				?>
+			</div>
 		</div>
+	</main>
+</div>
 <?php get_footer(); ?>
